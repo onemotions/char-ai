7a0884077e4cc785459cc71cb6df8185d7c34ecc
wjh <672428366@qq.com>
2023-05-16 15:20:02 +0800

    feat:创建工程

---
+++ char-interfaces/src/main/java/api/ApiApplication.java
@@ -0,0 +1,17 @@
+package api;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassNameApaApplication
+ * @Description TODO
+ * @Date2023/5/1615:09
+ **/
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+    }
+}
---
+++ char-interfaces/src/test/java/test/ApiTest.java
@@ -0,0 +1,9 @@
+package test;
+
+/**
+ * @ClassNameApiTest
+ * @Description TODO
+ * @Date2023/5/1615:15
+ **/
+public class ApiTest {
+}
